cf997a2d310603a72b59b99cc79dc260678f3a51
Helrho <armand-gab@hotmail.fr>
2023-06-26 14:58:33 +0200

    [ADD] BackOffice routes

--- src/controllers/backOfficeController.ts
+++ src/controllers/backOfficeController.ts
@@ -12,11 +12,35 @@ export const getOrders = async (req: Request, res: Response) => {
             correlationId: correlationId,
             replyTo: replyQueue
         };
-        await publishTopic('orders', 'get.orders', message);
+        await publishTopic('order', 'get.orders', message);
 
         const responses = await receiveResponses(replyQueue, correlationId, 1);
         if (!responses[0].success) {
             throw new Error('Cannot find orders');
+        }
+        res.status(200).json({message: responses[0].content});
+    }
+    catch (err) {
+        const errMessage = err instanceof Error ? err.message : 'An error occurred';
+        res.status(500).json({message: errMessage});
+    }
+};
+
+export const getPayments = async (req: Request, res: Response) => {
+    try {
+        const replyQueue = 'get.payments.reply';
+        const correlationId = uuidv4();
+        const message: MessageLapinou = {
+            success: true,
+            content: {id: (req as any).identityId},
+            correlationId: correlationId,
+            replyTo: replyQueue
+        };
+        await publishTopic('all', 'get.payments', message);
+
+        const responses = await receiveResponses(replyQueue, correlationId, 1);
+        if (!responses[0].success) {
+            throw new Error('Cannot find payments');
         }
         res.status(200).json({message: responses[0].content});
     }
--- src/routes/backofficeRoutes.ts
+++ src/routes/backofficeRoutes.ts
@@ -7,5 +7,6 @@ const router = express.Router();
 
 // Get
 router.get('/orders', authorize([IdentityType.SALES]), backOfficeController.getOrders);
+router.get('/payments', authorize([IdentityType.SALES]), backOfficeController.getPayments);
 
 export default router;
