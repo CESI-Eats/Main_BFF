a181e7b488b47d74e2bb5b0fbe5011004fdbb0b2
KikosS41 <killian.legendre41@gmail.com>
2023-06-27 11:45:05 +0200

    little fixes

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,5 +1,4 @@
 import {Request, Response} from 'express';
-import {IdentityType} from "../enums/identityType";
 import {v4 as uuidv4} from "uuid";
 import {MessageLapinou, publishTopic, receiveResponses} from "../services/lapinouService";
 
